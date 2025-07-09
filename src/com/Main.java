58bc322ddcb42fa7b4f60eda4bfe911a372a626f
OksanaTest <shevernia547@gmail.com>
2025-07-09 14:10:03 +0100

    comment

---
+++ src/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String  tests = "Changes";
+        System.out.println("First commit to GitHub");
+        System.out.println("Changes to add");
+        System.out.println();
+    }
+}
